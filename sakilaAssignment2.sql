c8c6ed66a7ab8824d1d2a736e47a9c7196ca4bbd
Har5haw <harshavardhan.gadireddy@zemosolabs.com>
2021-04-15 18:31:03 +0530

    minor bugs

--- sakilaAssignment2.sql
+++ sakilaAssignment2.sql
@@ -15,27 +15,33 @@ WHERE
     WHERE
       name = 'Documentary'
   );
+
+
+
+
   /* 2nd solution Find out the number of sci-fi movies rented by the store managed by Jon Stephens. */
 SELECT
-  COUNT(title) AS number_of_sci_fi_movies_rented_by_the_store_managed_by_Jon_Stephen
+  COUNT(*)
 FROM
-  film
-  INNER JOIN film_category fc ON fc.film_id = film.film_id
-  INNER JOIN category ON fc.category_id = category.category_id
+  category c,
+  film_category fc,
+  inventory i,
+  rental r,
+  staff s
 WHERE
-  category.name = 'Sci-Fi'
-  AND film.film_id IN (
-    SELECT
-      film_id
-    FROM
-      rental
-      INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
-      INNER JOIN store ON store.store_id = inventory.store_id
-      INNER JOIN staff ON staff.staff_id = store.manager_staff_id
-    WHERE
-      staff.first_name = 'Jon'
-      AND staff.last_name = 'Stephens'
-  );
+  c.name = 'Sci-fi'
+  AND c.category_id = fc.category_id
+  AND fc.film_id = i.film_id
+  AND i.inventory_id = r.inventory_id
+  AND r.staff_id = s.staff_id
+  AND s.first_name = 'Jon'
+  AND s.last_name = 'Stephens';
+
+
+
+
+
+
   /* 3rd solution Find out the total sales FROM Animation movies. */
 SELECT
   total_sales AS total_sales_from_Animation_movies
@@ -43,32 +49,42 @@ FROM
   sales_by_film_category
 WHERE
   sales_by_film_category.category = 'Animation';
+
+
+
+
+
   /* 4th solution Find out the top 3 rented category of movies by “PATRICIA JOHNSON”. */
 SELECT
-  category.name
+  fl.category
 FROM
-  rental
-  INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
-  INNER JOIN film_category ON film_category.film_id = inventory.film_id
-  INNER JOIN category ON category.category_id = film_category.category_id
+  film_list fl
 WHERE
-  rental.inventory_id IN (
+  fl.FID IN (
     SELECT
-      inventory_id
+      i.film_id
     FROM
-      inventory
-      INNER JOIN film_actor ON film_actor.film_id = inventory.film_id
-      INNER JOIN actor ON actor.actor_id = film_actor.actor_id
-    WHERE
-      actor.first_name = 'PATRICIA'
-      AND actor.last_name = 'JOHNSON'
+      rental r,
+      customer_list cl,
+      inventory i
+    where
+      cl.name = 'PATRICIA JOHNSON'
+      AND cl.ID = r.customer_id
+      AND r.inventory_id = i.inventory_id
   )
 GROUP BY
-  category.name
+  fl.category
 ORDER BY
-  count(category.name) DESC
-LIMIT
+  count(all fl.category) desc,
+  fl.category
+limit
   3;
+
+
+
+
+
+  
   /* 5th solution Find out the number of R rated movies rented by “SUSAN WILSON” */
 SELECT
   COUNT(*) as number_of_R_rated_movies_rented_by_SUSAN_WILSON
